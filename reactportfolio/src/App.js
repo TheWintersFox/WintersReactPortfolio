651ae0eaaa8787774e533153811f76d9e728bbc4
Luke Evans <lukeevanstax@gmail.com>
2020-10-07 01:07:52 -0400

    commit temporary routing current compiler failing to display separate web pages

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import Main from './components/main'
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 function App() {
     return (
@@ -9,18 +10,18 @@ function App() {
         <Layout fixedHeader>
             <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
                 <Navigation>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/aboutme">AboutMe</Link>
+                    <Link to="/contact">Contact</Link>
+                    <Link to="/resume">Resume</Link>
                 </Navigation>
             </Header>
             <Drawer title="Title">
                 <Navigation>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/aboutme">AboutMe</Link>
+                    <Link to="/contact">Contact</Link>
+                    <Link to="/resume">Resume</Link>
                 </Navigation>
             </Drawer>
             <Main/>
--- reactportfolio/src/components/main.js
+++ reactportfolio/src/components/main.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import LandingPage from './landingpage';
-// import About from './aboutme';
-// import Contact from './contact';
-// import Projects from './projects';
-// import Resume from './resume';
+import About from './aboutme';
+import Contact from './contact';
+import Projects from './projects';
+import Resume from './resume';
 import { BrowserRouter as Route, Switch } from "react-router-dom";
 
 const Main = () => (
@@ -11,6 +11,10 @@ const Main = () => (
         <div>
             <Switch>
                 <Route exact path="/" component={LandingPage} />
+                <Route exact path="/aboutme" component={About} />
+                <Route exact path="/contact" component={Contact} />
+                <Route exact path="/projects" component={Projects} />
+                <Route exact path="/resume" component={Resume} />
             </Switch>
         </div>
     </Route>
